8169bf81514b8494f029d76a0d1c8f01ce25a845
“Santosh <nyaksan@amazon.com>
2019-09-03 20:43:19 +0200

    Tree Data Conversion

---
+++ Tree.js
@@ -0,0 +1,458 @@
+const data = {
+    "objects": [
+        {
+            "object_name": "food_images/",
+            "size": 0
+        },
+        {
+            "object_name": "food_images/eggs/",
+            "size": 0
+        },
+        {
+            "object_name": "food_images/eggs/3_0.jpg",
+            "size": 38285
+        },
+        {
+            "object_name": "food_images/eggs/3_1.jpg",
+            "size": 47436
+        },
+        {
+            "object_name": "food_images/eggs/3_10.jpg",
+            "size": 48070
+        },
+        {
+            "object_name": "food_images/eggs/3_100.jpg",
+            "size": 26521
+        },
+        {
+            "object_name": "food_images/eggs/3_101.jpg",
+            "size": 52485
+        },
+        {
+            "object_name": "food_images/eggs/3_102.jpg",
+            "size": 55768
+        },
+        {
+            "object_name": "food_images/eggs/3_103.jpg",
+            "size": 56785
+        },
+        {
+            "object_name": "food_images/eggs/3_104.jpg",
+            "size": 26124
+        },
+        {
+            "object_name": "food_images/eggs/3_105.jpg",
+            "size": 48506
+        },
+        {
+            "object_name": "food_images/eggs/3_106.jpg",
+            "size": 50802
+        },
+        {
+            "object_name": "food_images/eggs/3_107.jpg",
+            "size": 55818
+        },
+        {
+            "object_name": "food_images/eggs/3_108.jpg",
+            "size": 42976
+        },
+        {
+            "object_name": "food_images/eggs/3_109.jpg",
+            "size": 54591
+        },
+        {
+            "object_name": "food_images/eggs/3_11.jpg",
+            "size": 54554
+        },
+        {
+            "object_name": "food_images/eggs/3_110.jpg",
+            "size": 54645
+        },
+        {
+            "object_name": "food_images/eggs/3_111.jpg",
+            "size": 44321
+        },
+        {
+            "object_name": "food_images/eggs/3_112.jpg",
+            "size": 46021
+        },
+        {
+            "object_name": "food_images/eggs/3_113.jpg",
+            "size": 41430
+        },
+        {
+            "object_name": "food_images/eggs/3_114.jpg",
+            "size": 44458
+        },
+        {
+            "object_name": "food_images/eggs/3_115.jpg",
+            "size": 58152
+        },
+        {
+            "object_name": "food_images/eggs/3_116.jpg",
+            "size": 37688
+        },
+        {
+            "object_name": "food_images/eggs/3_117.jpg",
+            "size": 53798
+        },
+        {
+            "object_name": "food_images/eggs/3_118.jpg",
+            "size": 57005
+        },
+        {
+            "object_name": "food_images/eggs/3_119.jpg",
+            "size": 36125
+        },
+        {
+            "object_name": "food_images/eggs/3_12.jpg",
+            "size": 53953
+        },
+        {
+            "object_name": "food_images/eggs/3_120.jpg",
+            "size": 35554
+        },
+        {
+            "object_name": "food_images/eggs/3_121.jpg",
+            "size": 26227
+        },
+        {
+            "object_name": "food_images/eggs/3_122.jpg",
+            "size": 49671
+        },
+        {
+            "object_name": "food_images/eggs/3_123.jpg",
+            "size": 62036
+        },
+        {
+            "object_name": "food_images/eggs/3_124.jpg",
+            "size": 35126
+        },
+        {
+            "object_name": "food_images/eggs/3_125.jpg",
+            "size": 44725
+        },
+        {
+            "object_name": "food_images/eggs/3_126.jpg",
+            "size": 51383
+        },
+        {
+            "object_name": "food_images/eggs/3_127.jpg",
+            "size": 32128
+        },
+        {
+            "object_name": "food_images/eggs/3_128.jpg",
+            "size": 52999
+        },
+        {
+            "object_name": "food_images/eggs/3_129.jpg",
+            "size": 46621
+        },
+        {
+            "object_name": "food_images/eggs/3_13.jpg",
+            "size": 36437
+        },
+        {
+            "object_name": "food_images/eggs/3_130.jpg",
+            "size": 54714
+        },
+        {
+            "object_name": "food_images/eggs/3_131.jpg",
+            "size": 49357
+        },
+        {
+            "object_name": "food_images/eggs/3_132.jpg",
+            "size": 39423
+        },
+        {
+            "object_name": "food_images/eggs/3_133.jpg",
+            "size": 47758
+        },
+        {
+            "object_name": "food_images/eggs/3_134.jpg",
+            "size": 38013
+        },
+        {
+            "object_name": "food_images/eggs/3_135.jpg",
+            "size": 55521
+        },
+        {
+            "object_name": "food_images/eggs/3_136.jpg",
+            "size": 42829
+        },
+        {
+            "object_name": "food_images/eggs/3_137.jpg",
+            "size": 47342
+        },
+        {
+            "object_name": "food_images/eggs/3_138.jpg",
+            "size": 27476
+        },
+        {
+            "object_name": "food_images/eggs/3_139.jpg",
+            "size": 42038
+        },
+        {
+            "object_name": "food_images/eggs/3_14.jpg",
+            "size": 55235
+        },
+        {
+            "object_name": "food_images/eggs/3_140.jpg",
+            "size": 56254
+        },
+        {
+            "object_name": "food_images/eggs/3_141.jpg",
+            "size": 49384
+        },
+        {
+            "object_name": "food_images/eggs/3_142.jpg",
+            "size": 45253
+        },
+        {
+            "object_name": "food_images/eggs/3_143.jpg",
+            "size": 34750
+        },
+        {
+            "object_name": "food_images/eggs/3_144.jpg",
+            "size": 65702
+        },
+        {
+            "object_name": "food_images/eggs/3_145.jpg",
+            "size": 61601
+        },
+        {
+            "object_name": "food_images/eggs/3_146.jpg",
+            "size": 48336
+        },
+        {
+            "object_name": "food_images/eggs/3_147.jpg",
+            "size": 55160
+        },
+        {
+            "object_name": "food_images/eggs/3_148.jpg",
+            "size": 34731
+        },
+        {
+            "object_name": "food_images/eggs/3_149.jpg",
+            "size": 46701
+        },
+        {
+            "object_name": "food_images/eggs/3_15.jpg",
+            "size": 52632
+        },
+        {
+            "object_name": "food_images/eggs/3_16.jpg",
+            "size": 52170
+        },
+        {
+            "object_name": "food_images/eggs/3_17.jpg",
+            "size": 42440
+        },
+        {
+            "object_name": "food_images/eggs/3_18.jpg",
+            "size": 48037
+        },
+        {
+            "object_name": "food_images/eggs/3_19.jpg",
+            "size": 52389
+        },
+        {
+            "object_name": "food_images/eggs/3_2.jpg",
+            "size": 54109
+        },
+        {
+            "object_name": "food_images/eggs/3_20.jpg",
+            "size": 58717
+        },
+        {
+            "object_name": "food_images/eggs/3_21.jpg",
+            "size": 41687
+        },
+        {
+            "object_name": "food_images/eggs/3_22.jpg",
+            "size": 56640
+        },
+        {
+            "object_name": "food_images/eggs/3_23.jpg",
+            "size": 42420
+        },
+        {
+            "object_name": "food_images/eggs/3_24.jpg",
+            "size": 53537
+        },
+        {
+            "object_name": "food_images/eggs/3_25.jpg",
+            "size": 58836
+        },
+        {
+            "object_name": "food_images/eggs/3_26.jpg",
+            "size": 37514
+        },
+        {
+            "object_name": "food_images/eggs/3_27.jpg",
+            "size": 24659
+        },
+        {
+            "object_name": "food_images/eggs/3_28.jpg",
+            "size": 45567
+        },
+        {
+            "object_name": "food_images/eggs/3_29.jpg",
+            "size": 41919
+        },
+        {
+            "object_name": "food_images/eggs/3_3.jpg",
+            "size": 38046
+        },
+        {
+            "object_name": "food_images/eggs/3_30.jpg",
+            "size": 40618
+        },
+        {
+            "object_name": "food_images/eggs/3_31.jpg",
+            "size": 46364
+        },
+        {
+            "object_name": "food_images/eggs/3_32.jpg",
+            "size": 61589
+        },
+        {
+            "object_name": "food_images/eggs/3_33.jpg",
+            "size": 43775
+        },
+        {
+            "object_name": "food_images/eggs/3_34.jpg",
+            "size": 37445
+        },
+        {
+            "object_name": "food_images/eggs/3_35.jpg",
+            "size": 60532
+        },
+        {
+            "object_name": "food_images/eggs/3_36.jpg",
+            "size": 46218
+        },
+        {
+            "object_name": "food_images/eggs/3_37.jpg",
+            "size": 41591
+        },
+        {
+            "object_name": "food_images/eggs/3_38.jpg",
+            "size": 77351
+        },
+        {
+            "object_name": "food_images/eggs/3_39.jpg",
+            "size": 37631
+        },
+        {
+            "object_name": "food_images/eggs/3_4.jpg",
+            "size": 40715
+        },
+        {
+            "object_name": "food_images/eggs/3_40.jpg",
+            "size": 36795
+        },
+        {
+            "object_name": "food_images/eggs/3_41.jpg",
+            "size": 41117
+        },
+        {
+            "object_name": "food_images/eggs/3_42.jpg",
+            "size": 55916
+        },
+        {
+            "object_name": "food_images/eggs/3_43.jpg",
+            "size": 36900
+        },
+        {
+            "object_name": "food_images/eggs/3_44.jpg",
+            "size": 53855
+        },
+        {
+            "object_name": "food_images/eggs/3_45.jpg",
+            "size": 50218
+        },
+        {
+            "object_name": "food_images/eggs/3_46.jpg",
+            "size": 56036
+        },
+        {
+            "object_name": "food_images/eggs/3_47.jpg",
+            "size": 56940
+        },
+        {
+            "object_name": "food_images/eggs/3_48.jpg",
+            "size": 58762
+        },
+        {
+            "object_name": "food_images/eggs/3_49.jpg",
+            "size": 50711
+        },
+        {
+            "object_name": "food_images/eggs/3_5.jpg",
+            "size": 56041
+        },
+        {
+            "object_name": "food_images/eggs/3_50.jpg",
+            "size": 57813
+        },
+        {
+            "object_name": "food_images/eggs/3_51.jpg",
+            "size": 31622
+        }
+    ]
+}
+
+var arr = [] //your array;
+var tree = {};
+
+function addnode(obj){
+  var splitpath = obj.object_name.replace(/^\/|\/$/g, "").split('/');
+  var ptr = tree;
+  for (i=0;i<splitpath.length;i++)
+  {
+    node = { name: splitpath[i],
+    type: 'directory'};
+    if(i == splitpath.length-1)
+    {node.size = obj.size;node.type = obj.type;}
+    ptr[splitpath[i]] = ptr[splitpath[i]]||node;
+    ptr[splitpath[i]].childrenList=ptr[splitpath[i]].childrenList ||{};
+    ptr[splitpath[i]].children = Object.values(ptr[splitpath[i]].childrenList)
+    ptr=ptr[splitpath[i]].childrenList;
+  }    
+}
+
+data.objects.map(addnode);
+
+const tree1 = {
+    name: 'tree',
+    value: 'tree',
+    children: [
+      {name: 'master', value: 'master', masterSelect: true},
+      {name: 'one', value: 'one-1'},
+      {
+        name: 'two',
+        value: 'two-1',
+        children: [
+          {name: 'master', value: 'master-2', masterSelect: true},
+          {
+            name: 'child-1',
+            value: 'four-1',
+            children: [
+              {name: 'child-1-1', value: 'descendant-1-1'},
+              {name: 'child-1-2', value: 'descendant-2-1'}
+            ]
+          },
+          {name: 'child-2', value: 'five-1'}
+        ]
+      },
+      {name: 'four', value: 'forty-four-1'},
+      {name: 'five', value: 'fifty-six-1'},
+      {name: 'six', value: 'six-1'},
+      {name: 'seven', value: 'seven-1'},
+      {name: 'eight', value: 'eight-1'},
+      {name: 'nine', value: 'nine-1'}
+    ]
+  };
+
+console.log(tree);
